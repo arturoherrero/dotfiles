c0a2e15fd9b314cd2e36305bbe35de2c3b8ec433
Arturo Herrero <arturo.herrero@gmail.com>
2016-10-12 10:35:23 +0100

    Alias for rackup

--- system/ruby.sh
+++ system/ruby.sh
@@ -2,6 +2,7 @@
 
 alias be="bundle exec"
 alias guard="bundle exec guard"
+alias rackup="bundle exec rackup"
 alias rails="bundle exec rails"
 alias rake="bundle exec rake"
 alias rake_db_migrate="rake db:migrate && rake db:test:prepare"
