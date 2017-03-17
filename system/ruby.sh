ebcc83df67230e427ff1c51f082eea0a67564d53
Arturo Herrero <arturo.herrero@gmail.com>
2017-03-17 16:06:10 +0000

    Update Ruby aliases

--- system/ruby.sh
+++ system/ruby.sh
@@ -6,12 +6,8 @@ alias guard="bundle exec guard"
 alias rackup="bundle exec rackup"
 alias rails="bundle exec rails"
 alias rake="bundle exec rake"
-alias rake_db_migrate="rake db:migrate && rake db:test:prepare"
-
 alias rspec="bundle exec rspec"
-alias rspec_prepare="rake db:test:prepare"
-alias rspec_pararell="rake parallel:spec"
-alias rspec_pararell_prepare="rake parallel:prepare"
+alias rspec_parallel="rake parallel:spec"
 
 alias jekyll="jekyll server --watch --future"
 
