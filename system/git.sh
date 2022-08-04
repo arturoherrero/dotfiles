arturoherrero/dotfiles

commit bb2e527f2a7cbc32d6cfe6c47ece6622ff6a6e58

Auto setup remote

system/git.sh
@@ -4,22 +4,6 @@ __system_git_current_branch() {
   git branch --show-current
 }
 
-# $ git push
-# Prevent error when pushing to a remote repository without upstream branch.
-# Push the current branch and set the remote as upstream.
-__system_git_push_creating_upstream_branch() {
-  stderr=$(command git "$@" 2> >(tee /dev/stderr | head -n 1))
-  current_branch=$(__system_git_current_branch)
-  error="fatal: The current branch $current_branch has no upstream branch."
-
-  if [ "$stderr" == "$error" ]; then
-    echo "↝ git push --set-upstream origin $current_branch"
-    git push --set-upstream origin "$current_branch"
-  else
-    git "$@"
-  fi
-}
-
 # git push -f
 # git push --force
 # Show a confirmation when forcing a push on master branch.
@@ -29,8 +13,6 @@ __system_git_push_confirmation_master_branch_force() {
     if [[ $REPLY =~ ^(yes|y|Y)$ ]]; then
       git "$@"
     fi
-  else
-    __system_git_push_creating_upstream_branch "$@"
   fi
 }
 
@@ -38,8 +20,6 @@ __system_git_push_confirmation_master_branch_force() {
 __system_git_push() {
   if [[ "$*" == "push -f"* || "$*" == "push --force"* ]]; then
     __system_git_push_confirmation_master_branch_force "$@"
-  else
-    __system_git_push_creating_upstream_branch "$@"
   fi
 }
 

.gitconfig
@@ -46,6 +46,7 @@
   autocorrect = 1
 [push]
   default = simple
+  autoSetupRemote = true
 [pull]
   ff = only
 [merge]

commit 1d1a2ce075d8f9ed8ec59d5014c414c88bb47af9

Fix git push
This bug was introduced by bb2e527.

system/git.sh
@@ -13,6 +13,8 @@ __system_git_push_confirmation_master_branch_force() {
     if [[ $REPLY =~ ^(yes|y|Y)$ ]]; then
       git "$@"
     fi
+  else
+    git "$@"
   fi
 }
 
@@ -20,6 +22,8 @@ __system_git_push_confirmation_master_branch_force() {
 __system_git_push() {
   if [[ "$*" == "push -f"* || "$*" == "push --force"* ]]; then
     __system_git_push_confirmation_master_branch_force "$@"
+  else
+    git "$@"
   fi
 }
 