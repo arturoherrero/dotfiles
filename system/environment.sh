25f946a31f9b7312520711e5c2c8bd86f71ad1c6
Arturo Herrero <arturo.herrero@gmail.com>
2022-08-06 10:26:54 +0100

    Add support for Bash completion 2 under new path

--- system/environment.sh
+++ system/environment.sh
@@ -51,3 +51,4 @@ unset MAILCHECK
 
 # Bash completion 2
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
