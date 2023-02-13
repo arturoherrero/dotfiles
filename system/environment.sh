9f00d334e57bf62ad1a668075f20068f7a5f41ae
Arturo Herrero <arturo.herrero@gmail.com>
2023-02-13 17:20:00 +0000

    Fix Bash completion

--- system/environment.sh
+++ system/environment.sh
@@ -50,4 +50,4 @@ export EDITOR=vim
 unset MAILCHECK
 
 # Bash completion 2
-[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
